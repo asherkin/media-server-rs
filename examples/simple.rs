d89f74bdf0ef2c4d8173f714a0a4005edcdea53b
Asher Baker <asherkin@limetech.io>
2020-11-29 01:23:04 +0000

    First pass at a low-level public API

---
+++ examples/simple.rs
@@ -0,0 +1,83 @@
+use std::time::Duration;
+
+use futures::channel::oneshot;
+use futures::future::Either;
+use futures_timer::Delay;
+
+use media_server::{
+    DtlsConnectionHash,
+    DtlsIceTransportDtlsState,
+    DtlsIceTransportListener,
+    Properties,
+    Result,
+    RtpBundleTransport,
+    RtpBundleTransportConnection,
+};
+
+struct WaitForConnectionListener(Option<oneshot::Sender<()>>);
+
+impl DtlsIceTransportListener for WaitForConnectionListener {
+    fn on_dtls_state_changed(&mut self, state: DtlsIceTransportDtlsState) {
+        if state == DtlsIceTransportDtlsState::Connected {
+            // Ignore failure, panicking in here is an abort.
+            if let Some(sender) = self.0.take() {
+                let _ = sender.send(());
+            }
+        }
+    }
+}
+
+struct TestTransport {
+    transport: RtpBundleTransport,
+    connection: RtpBundleTransportConnection,
+    receiver: oneshot::Receiver<()>,
+}
+
+fn create_test_transport(local_username: &str, remote_username: &str, remote_dtls_setup: &str) -> Result<TestTransport> {
+    let fingerprint = media_server::get_certificate_fingerprint(DtlsConnectionHash::Sha256)?;
+
+    let transport = RtpBundleTransport::new(None)?;
+
+    let properties = Properties::new();
+    properties.set_string("ice.localUsername", local_username);
+    properties.set_string("ice.localPassword", "");
+    properties.set_string("ice.remoteUsername", remote_username);
+    properties.set_string("ice.remotePassword", "");
+    properties.set_string("dtls.setup", remote_dtls_setup);
+    properties.set_string("dtls.hash", "SHA-256");
+    properties.set_string("dtls.fingerprint", &fingerprint);
+    properties.set_bool("disableSTUNKeepAlive", true);
+    properties.set_string("srtpProtectionProfiles", "");
+
+    let username = local_username.to_owned() + ":" + remote_username;
+    let connection = transport.add_ice_transport(username.as_str(), &properties)?;
+
+    let (sender, receiver) = oneshot::channel();
+    connection.set_listener(WaitForConnectionListener(Some(sender)));
+
+    Ok(TestTransport { transport, connection, receiver })
+}
+
+fn main() -> Result<()> {
+    media_server::library_init()?;
+
+    let one = create_test_transport("one", "two", "active")?;
+    let two = create_test_transport("two", "one", "passive")?;
+
+    two.connection.add_remote_candidate("127.0.0.1", one.transport.get_local_port());
+
+    futures::executor::block_on(async {
+        let connected = futures::future::try_join(one.receiver, two.receiver);
+        let timeout = Delay::new(Duration::from_secs(5));
+
+        match futures::future::select(connected, timeout).await {
+            Either::Left((Ok(_), _)) => Ok(()),
+            Either::Left((Err(_), _)) => Err("failed to read from channel"),
+            Either::Right(_) => Err("connection timed out"),
+        }
+    })?;
+
+    println!("WebRTC transports are connected!");
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,86 @@ pub fn library_init() -> Result<()> {
 
     Ok(())
 }
+
+pub enum DtlsConnectionHash {
+    Sha1,
+    Sha224,
+    Sha256,
+    Sha384,
+    Sha512,
+    UnknownHash,
+}
+
+impl Into<bridge::DtlsConnectionHash> for DtlsConnectionHash {
+    fn into(self) -> bridge::DtlsConnectionHash {
+        match self {
+            DtlsConnectionHash::Sha1 => bridge::DtlsConnectionHash::SHA1,
+            DtlsConnectionHash::Sha224 => bridge::DtlsConnectionHash::SHA224,
+            DtlsConnectionHash::Sha256 => bridge::DtlsConnectionHash::SHA256,
+            DtlsConnectionHash::Sha384 => bridge::DtlsConnectionHash::SHA384,
+            DtlsConnectionHash::Sha512 => bridge::DtlsConnectionHash::SHA512,
+            DtlsConnectionHash::UnknownHash => bridge::DtlsConnectionHash::UNKNOWN_HASH,
+        }
+    }
+}
+
+pub fn get_certificate_fingerprint(hash: DtlsConnectionHash) -> Result<String> {
+    let fingerprint = bridge::dtls_connection_get_certificate_fingerprint(hash.into())?;
+    Ok(fingerprint)
+}
+
+pub struct Properties(cxx::UniquePtr<bridge::PropertiesFacade>);
+
+impl Properties {
+    pub fn new() -> Self {
+        Self(bridge::new_properties())
+    }
+
+    pub fn set_int(&self, key: &str, value: i32) {
+        self.0.set_int(key, value);
+    }
+
+    pub fn set_bool(&self, key: &str, value: bool) {
+        self.0.set_bool(key, value);
+    }
+
+    pub fn set_string(&self, key: &str, value: &str) {
+        self.0.set_string(key, value);
+    }
+}
+
+pub use bridge::DtlsIceTransportListener;
+
+pub type DtlsIceTransportDtlsState = bridge::DtlsIceTransportDtlsState;
+
+pub struct RtpBundleTransportConnection(cxx::UniquePtr<bridge::RtpBundleTransportConnectionFacade>);
+
+impl RtpBundleTransportConnection {
+    pub fn set_listener(&self, listener: impl DtlsIceTransportListener + 'static) {
+        let listener = bridge::DtlsIceTransportListenerRustAdapter::from(listener);
+        self.0.set_listener(Box::new(listener));
+    }
+
+    pub fn add_remote_candidate(&self, ip: &str, port: u16) {
+        self.0.add_remote_candidate(ip, port);
+    }
+}
+
+pub struct RtpBundleTransport(cxx::UniquePtr<bridge::RtpBundleTransportFacade>);
+
+impl RtpBundleTransport {
+    pub fn new(port: Option<u16>) -> Result<Self> {
+        let port = port.unwrap_or(0);
+        let transport = bridge::new_rtp_bundle_transport(port)?;
+        Ok(Self(transport))
+    }
+
+    pub fn get_local_port(&self) -> u16 {
+        self.0.get_local_port()
+    }
+
+    pub fn add_ice_transport(&self, username: &str, properties: &Properties) -> Result<RtpBundleTransportConnection> {
+        let connection = self.0.add_ice_transport(username, &properties.0)?;
+        Ok(RtpBundleTransportConnection(connection))
+    }
+}
